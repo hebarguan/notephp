3acb3a243a71925a6e69ef577344ed50bddc864c
hebarguan <hebarguan@hotmail.com>
2016-01-19 14:25:54 +0800

    load newfile

---
+++ notephp/common/funcions.php
@@ -0,0 +1,10 @@
+<?php
+ /*
+  * This is a pulic common function library
+  * functions.php will be loading in first initialize
+  * can be use in web root !
+  * from hebarguan editer hebarguan@hotmail.com
+  */
+
+
+?>
---
+++ notephp/notephp.init.php
@@ -0,0 +1,9 @@
+<?php
+ /*
+  * designed by hebarguan in 2016-1-19
+  * user email hebarguan@hotmail.com
+  * notephp is a easy phpmvc !
+  */
+
+
+?>
