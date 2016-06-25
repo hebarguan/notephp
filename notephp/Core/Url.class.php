3ddac9554c53ba196d678ad172d4b5539712b156
hebarguan <hebarguan@hotmail.com>
2016-06-26 01:37:22 +0800

    更改路由类命名规则

--- notephp/Core/Url.class.php
+++ notephp/Core/Url.class.php
@@ -7,34 +7,33 @@
 class Url
 {
     // 定义路由模式
-    private $UrlMode         = "";
+    private $urlMode         = "";
     // 请求路由
-    private $RequestUri      = "";
+    private $requestUri      = "";
     // 完整路由
-    private $FullUrl         = "";
+    private $fullUrl         = "";
     // 路由重写
-    private $UrlRewrite      = "";
+    private $urlRewrite      = "";
     // 路由重写
-    private $UrlMap          = "";
+    private $urlMap          = "";
     //　定义GET参数数组
-    private $HttpBuildQuery  = array();
+    private $httpBuildQuery  = array();
     // 当前路由请求的模块
-    private $RequestModule   = "";
+    private $requestModule   = "";
     // 控制器名
-    private $Controller      = "";
+    private $controller      = "";
     // 操作方法
-    private $Action          = "";
+    private $action          = "";
     // 路由后缀
-    private $UrlSuffix       = "";
+    private $urlSuffix       = "";
     // 定义构造函数
     public function __construct()
     {
-
-        $this->UrlMode     = C('URL_MODE');
-        $this->RequestUri  = $_SERVER['REQUEST_URI'];
-        $this->UrlRewrite  = C('URL_REWRITE_RULES');
-        $this->UrlMap      = C('URL_MAP_RULES');
-        $this->UrlSuffix   = C('URL_STATIC_SUFFIX');
+        $this->urlMode     = C('URL_MODE');
+        $this->requestUri  = $_SERVER['REQUEST_URI'];
+        $this->urlRewrite  = C('URL_REWRITE_RULES');
+        $this->urlMap      = C('URL_MAP_RULES');
+        $this->urlSuffix   = C('URL_STATIC_SUFFIX');
         // 是否开启session驱动
         if (C("SESSION_DRIVER_OPEN")) {
              /*
@@ -49,14 +48,14 @@ class Url
     // 开始路由处理
     public function start ()
     {
-        $this->FullUrl = $this->checkUrlSuffix(str_replace("/index.php?", "", $this->RequestUri));
+        $this->fullUrl = $this->checkUrlSuffix(str_replace("/index.php?", "", $this->requestUri));
         // 检查是否开启路由映射
         // 是否开启路由重写,只使用模式１
-        if (!empty($this->UrlRewrite) AND 1 == $this->UrlMode) {
+        if (!empty($this->urlRewrite) AND 1 == $this->urlMode) {
             // 直接正则匹配
-            $pattern = array_keys($this->UrlRewrite);
-            $replacement = array_values($this->UrlRewrite);
-            $this->FullUrl = preg_replace($pattern, $replacement, $this->FullUrl);
+            $pattern = array_keys($this->urlRewrite);
+            $replacement = array_values($this->urlRewrite);
+            $this->fullUrl = preg_replace($pattern, $replacement, $this->fullUrl);
         }
         // 检测是否以开启自动路由隐藏模块
         if (C('URL_HIDE_MODULE')) {
@@ -74,9 +73,9 @@ class Url
              * 判断子域名是否部署成功
              */
             if (isset($GLOBALS['PROJECT_REQUEST_MODULE'])) {
-                $this->FullUrl = __ROOT__.$GLOBALS['PROJECT_REQUEST_MODULE'].$this->FullUrl;
+                $this->fullUrl = __ROOT__.$GLOBALS['PROJECT_REQUEST_MODULE'].$this->fullUrl;
             } else {
-                $this->FullUrl = __ROOT__.APP_NAME.$this->FullUrl;
+                $this->fullUrl = __ROOT__.APP_NAME.$this->fullUrl;
             }
         }
 
@@ -86,10 +85,10 @@ class Url
         * 判断处理后的模块／控制器／操作方法是否为空
         * 以全球变量声明模块
         */
-        $GLOBALS['PROJECT_REQUEST_MODULE'] = ucfirst($this->RequestModule ? $this->RequestModule : APP_NAME) ;
-        $this->RequestModule = $GLOBALS['PROJECT_REQUEST_MODULE'] ;
-        $GLOBALS['PROJECT_REQUEST_CONTROLLER'] = ucfirst($this->Controller);
-        $GLOBALS['PROJECT_REQUEST_ACTION']     = $this->Action;
+        $GLOBALS['PROJECT_REQUEST_MODULE'] = ucfirst($this->requestModule ? $this->requestModule : APP_NAME) ;
+        $this->requestModule = $GLOBALS['PROJECT_REQUEST_MODULE'] ;
+        $GLOBALS['PROJECT_REQUEST_CONTROLLER'] = ucfirst($this->controller);
+        $GLOBALS['PROJECT_REQUEST_ACTION']     = $this->action;
        /*
         * 加载特定模块的函数库
         * 以遍历的方式加载目录函数文件
@@ -104,13 +103,13 @@ class Url
             }
         }
         // 将数据交给控制器驱动处理
-        ControllerDriver::work($this->RequestModule, $this->Controller, $this->Action);
+        ControllerDriver::work($this->requestModule, $this->controller, $this->action);
     }
     // 检测是否有路由后缀
     public function checkUrlSuffix($url) 
     {
-        $suffixLen = strlen($this->UrlSuffix) + 1;
-        if (substr($url, -$suffixLen) === ".{$this->UrlSuffix}") {
+        $suffixLen = strlen($this->urlSuffix) + 1;
+        if (substr($url, -$suffixLen) === ".{$this->urlSuffix}") {
             $fullUrl =  substr($url, 0, strlen($url) - $suffixLen);
             return $fullUrl;
         }
@@ -119,7 +118,7 @@ class Url
     // 路由匹配返回完整路由
     public function urlPattern()
     {
-        if (!empty($this->UrlMap) && ($this->UrlMode == 2)) {
+        if (!empty($this->urlMap) && ($this->urlMode == 2)) {
             // 整合路由映射，返回完整路由
             $maarr = array();
             $patternString = "/(\/[^:]+)(:[^\/]+)";
@@ -128,25 +127,25 @@ class Url
                 $patternString .= "(\/:[^\/]+)?";
                 $patternFields .= "(\/[^/]+)?";
             }
-            foreach ($this->UrlMap as $map => $val) {
+            foreach ($this->urlMap as $map => $val) {
                 if (!preg_match_all($patternString."/", $map, $matches, PREG_SET_ORDER)) continue;
                 $maarr = $matches[0];
                 if (!empty($maarr)) {
                     $UrlPattern = "|({$maarr[1]})([^/]+)".$patternFields."|";
-                    if (!preg_match_all($UrlPattern, $this->FullUrl, $valMatches, PREG_SET_ORDER)) continue;
+                    if (!preg_match_all($UrlPattern, $this->fullUrl, $valMatches, PREG_SET_ORDER)) continue;
                     for ($i = 2; $i < count($maarr); $i++) {
                         $GetKey = preg_replace("/[:|\/]*([\S]+)$/", "$1", $maarr[$i]);
                         $GetVal = preg_replace("/[:|\/]*([\S]+)$/", "$1", $valMatches[0][$i]);
-                        $this->HttpBuildQuery[$GetKey] = $GetVal;
+                        $this->httpBuildQuery[$GetKey] = $GetVal;
                     }
                     // 模式２的路由方式
                     $ModeQueryString = $val;
-                    while (list($k ,$v) = each($this->HttpBuildQuery) ) {
+                    while (list($k ,$v) = each($this->httpBuildQuery) ) {
                         if (!($k AND $v))  continue;
                         $ModeQueryString .= "/".$k."/".$v;
                     }
                     // 返回模式２的完整路由
-                    $this->FullUrl = $ModeQueryString;
+                    $this->fullUrl = $ModeQueryString;
                 }
             }
         }
@@ -154,17 +153,17 @@ class Url
     // 路由数据分割
     public function urlDataSpilt()
     {
-        switch ($this->UrlMode) {
+        switch ($this->urlMode) {
         case 1 :
-            $spiltQuery = explode("?", $this->FullUrl) ;
+            $spiltQuery = explode("?", $this->fullUrl) ;
             $index_handle = array_shift($spiltQuery);
             // 获取控制器与动作
             $queryArr = explode("/", $index_handle);
             array_shift($queryArr);
             $C_A = $queryArr;
-            $this->RequestModule = !empty($C_A[0]) ? $C_A[0] : null;
-            $this->Controller    = !empty($C_A[1]) ? $C_A[1] : C('DEFAULT_CONTROLLER') ;
-            $this->Action        = !empty($C_A[2]) ? $C_A[2] : C("DEFAULT_METHOD");
+            $this->requestModule = !empty($C_A[0]) ? $C_A[0] : null;
+            $this->controller    = !empty($C_A[1]) ? $C_A[1] : C('DEFAULT_CONTROLLER') ;
+            $this->action        = !empty($C_A[2]) ? $C_A[2] : C("DEFAULT_METHOD");
             // 只截取第一个?
             if (!empty($spiltQuery)) {
                 $GetData = join("?", $spiltQuery);
@@ -176,17 +175,17 @@ class Url
             }
             break;
         case 2 :
-            $queryArr = explode("/", $this->FullUrl);
+            $queryArr = explode("/", $this->fullUrl);
             array_shift($queryArr);
-            $this->RequestModule = !empty($queryArr[0]) ? $queryArr[0] : null;
-            $this->Controller    = !empty($queryArr[1]) ? $queryArr[1] : C("DEFAULT_CONTROLLER");
-            $this->Action        = !empty($queryArr[2]) ? $queryArr[2] : C('DEFAULT_METHOD');
+            $this->requestModule = !empty($queryArr[0]) ? $queryArr[0] : null;
+            $this->controller    = !empty($queryArr[1]) ? $queryArr[1] : C("DEFAULT_CONTROLLER");
+            $this->action        = !empty($queryArr[2]) ? $queryArr[2] : C('DEFAULT_METHOD');
             for ($k = 3; $k < count($queryArr); $k+=2) {
-                $this->HttpBuildQuery[$queryArr[$k]] = $queryArr[$k+1];
+                $this->httpBuildQuery[$queryArr[$k]] = $queryArr[$k+1];
             }
             // 返回$_GET数据
-            if (!empty($this->HttpBuildQuery)) {
-                $_GET = array_unique($this->HttpBuildQuery);
+            if (!empty($this->httpBuildQuery)) {
+                $_GET = array_unique($this->httpBuildQuery);
             } else {
                 $_GET = array();
             }
