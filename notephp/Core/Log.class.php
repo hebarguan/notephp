c3cb4c9811fc45262c743a691da49db142c0266b
hebarguan <guanhuaihai@gmail.com>
2018-01-17 11:18:28 +0800

    修改日志类

--- notephp/Core/Log.class.php
+++ notephp/Core/Log.class.php
@@ -60,7 +60,7 @@ class Log
         $logFile = $logPath.'/log-'.date('Y-m-d').'.php';
         if ( ! is_file($logFile)) {
             self::$LogFileHandle = fopen($logFile,"a+");
-            fwrite(self::$LogFileHandle, '<?php </br>');
+            fwrite(self::$LogFileHandle, '<?php ');
         }
         self::$LogFileHandle = fopen($logFile,"a+");
         fwrite(self::$LogFileHandle, $errorMsg);
